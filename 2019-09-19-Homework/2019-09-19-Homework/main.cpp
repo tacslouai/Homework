0a43d13261139fb13e19be260aee45208bfd189c
tacslouai <55443256+tacslouai@users.noreply.github.com>
2019-09-19 08:30:37 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/main.cpp
@@ -0,0 +1,11 @@
+// Name: Louai Hammad
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include <iostream>
+int main()
+{
+	std::cout <<"Name: Louai Hammad \nClass Schedule \nPeriod 1 Intro to Computer Science \nPeriod 2 Earth Science \nPeriod 3 Physical Education \nPeriod 4 Health \nPeriod 5 Algebra II Honors \nPeriod 6 English 10 Honors \nPeriod 7 Lunch \nPeriod 8 Global History I \nPeriod 9 Spanish II \nPeriod 10 House";
+}
